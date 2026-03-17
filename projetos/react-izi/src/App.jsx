6f65ae53d9a8a16e2c1cf1341b2a9ea207ec5631
Nocturnnn <nocturn2352@gmail.com>
2026-03-17 10:47:09 -0300

    commit

--- projetos/react-izi/src/App.jsx
+++ projetos/react-izi/src/App.jsx
@@ -1,26 +1,15 @@
-import { useState } from "react";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
   // useState é um hook do React que permite adicionar estado a um componente funcional. 
   // Ele retorna um array com dois elementos: o valor atual do estado e uma função para atualizá-lo.
-  const [contador, setContador] = useState(0);
-
-  const nome = "Mateus";
 
   return (
-    <div className="App">
-      <h1>Olá, mundo!</h1>
-      <h2>Meu nome é {nome}.</h2>
-
-      <button
-        onClick={() => {
-          setContador(contador + 1);
-        }}
-      >
-        Click me {contador}
-      </button>
-
-      <p>Este é o meu aplicativo React.</p>
+    <div>
+      <h1 className="text-purple-500 text-lg font-bold">Gerenciador de Tarefas</h1>
+      <Tasks />
+      <AddTask />
     </div>
   );
 }
---
+++ projetos/react-izi/src/components/AddTask.jsx
@@ -0,0 +1,7 @@
+function AddTask() {
+    return (
+        <h1>AddTask</h1>
+    )
+}
+
+export default AddTask;
