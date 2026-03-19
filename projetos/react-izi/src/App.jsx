661abfdf93ff9a8ba88895ec2ffc538d3ee15042
Nocturnnn <nocturn2352@gmail.com>
2026-03-19 10:56:47 -0300

    commit

--- projetos/react-izi/src/App.jsx
+++ projetos/react-izi/src/App.jsx
@@ -1,15 +1,46 @@
+import { useState } from "react";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 function App() {
-  // useState é um hook do React que permite adicionar estado a um componente funcional. 
-  // Ele retorna um array com dois elementos: o valor atual do estado e uma função para atualizá-lo.
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Comprar leite",
+      description: "Comprar leite no supermercado",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar React",
+      description: "Assistir aulas e praticar exercícios",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Fazer exercícios",
+      description: "Fazer exercícios físicos para manter a saúde",
+      isCompleted: false,
+    },
+  ]);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+      return task; // não altera as outras tarefas, apenas a que foi clicada
+    });
+    setTasks(newTasks); // atualização do estado com a nova lista de tarefas
+  }
 
   return (
-    <div>
-      <h1 className="text-purple-500 text-lg font-bold">Gerenciador de Tarefas</h1>
-      <Tasks />
-      <AddTask />
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-125">
+        <h1 className="text-3xl font-bold text-center mb-6">To Do List</h1>
+        <AddTask />
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
+      </div>
     </div>
   );
 }
--- projetos/react-izi/src/components/Tasks.jsx
+++ projetos/react-izi/src/components/Tasks.jsx
@@ -1,7 +1,23 @@
-function Tasks() {
-    return (
-        <h1>Tasks</h1>
-    )
+import { ChevronRightIcon } from "lucide-react";
+
+function Tasks(props) {
+  return (
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+      {props.tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+          <button
+            onClick={() => props.onTaskClick(task.id)}
+            className="bg-slate-400 text-white text-left w-full p-2 rounded-md"
+          >
+            {task.isCompleted ? <s>{task.title}</s> : task.title}
+          </button>
+          <button className="ml-2 p-2 rounded-md hover:bg-slate-300">
+            <ChevronRightIcon className="text-slate-400" />
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default Tasks;
